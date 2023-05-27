5b8f59458b24f6c851ec40ac9711c5b20dc1b10f
semantic-void <90981866+semantic-void@users.noreply.github.com>
2023-05-27 18:42:12 +0530

    add build.js

--- build.js
+++ build.js
@@ -1,26 +1,13 @@
 const fs = require('fs');
 const path = require('path');
-const handlebars = require('handlebars');
-
 const viewsDir = path.join(__dirname, 'views');
-const outputDir = path.join(__dirname, 'public', 'views');
 
-// Read the Handlebars templates from the views directory
-fs.readdirSync(viewsDir).forEach(file => {
-  const filePath = path.join(viewsDir, file);
-  const template = fs.readFileSync(filePath, 'utf-8');
+// Read all .hbs files in the views directory
+const files = fs.readdirSync(viewsDir).filter((file) => file.endsWith('.hbs'));
 
-  // Compile the template
-  const compiledTemplate = handlebars.compile(template);
+// Create the views.js file with the list of .hbs files
+const viewsFilePath = path.join(__dirname, 'public/views.js');
+const viewsContent = `module.exports = ${JSON.stringify(files)};`;
+fs.writeFileSync(viewsFilePath, viewsContent);
 
-  // Generate the output file path
-  const outputFile = path.join(outputDir, file.replace('.hbs', '.html'));
-
-  // Render the template and write the output file
-  const output = compiledTemplate();
-  fs.writeFileSync(outputFile, output);
-
-  console.log(`Template ${file} compiled successfully.`);
-});
-
-console.log('Build process completed.');
+console.log('Views list generated successfully.');
---
+++ public/views.js
@@ -0,0 +1,1 @@
+module.exports = ["dashboard.hbs","login.hbs"];
